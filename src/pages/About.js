b6926aa9e366ae641bc4ead52bc7afb43ca91dae
ALP2001 <123103998+ALP2001@users.noreply.github.com>
2023-01-19 22:29:28 +0530

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container padding">
+      <div className="col-md-12">
+        <div className="row mx-0">
+          <p>
+         
+An newsletter is a periodical email sent by a person or business containing news, updates, curated content from the topic that you signed up for.
+            
+
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
